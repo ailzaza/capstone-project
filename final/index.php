63e1ac5741258e98d5ca7885a289e3ef95453a0f
ailzaza <135003494+ailzaza@users.noreply.github.com>
2023-06-17 01:03:01 +0700

    Update index.php

--- final/index.php
+++ final/index.php
@@ -5,6 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <!-- Include Bootstrap CSS -->
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css"
+    />
     <title>Sign up</title>
 </head>
 <body>
